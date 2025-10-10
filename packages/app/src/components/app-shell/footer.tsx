30ed7599e65f62e8d534294015f2a76031d6ee64
Denis Marushchak <denismar322@gmail.com>
2025-10-10 19:04:14 +1000

    fix: update footer text and link styling in app shell

--- packages/app/src/components/app-shell/footer.tsx
+++ packages/app/src/components/app-shell/footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
 	return (
 		<h2 className="text-center text-xs text-gray-500 mb-5 font-medium">
-			Built with love ️♥️ by
+			Built with by{" "}
 			<a
 				href="https://dmrk.dev"
 				target="_blank"
-				className="px-1 text-sky-900"
+				className="text-slate-800"
 				rel="noopener"
 			>
 				dmrk.dev
